125d9fffbd7490d0a4d79452a62b917c297aa20a
Vedant Andhale <90826179+VedantAndhale@users.noreply.github.com>
2023-02-21 16:11:02 +0530

    upt

--- assets/js/popover.js
+++ assets/js/popover.js
@@ -5,14 +5,14 @@ function htmlToElement(html) {
   return template.content.firstChild
 }
 
-function initPopover(baseURL, useContextualBacklinks) {
+function initPopover(baseURL, useContextualBacklinks, renderLatex) {
   const basePath = baseURL.replace(window.location.origin, "")
   fetchData.then(({ content }) => {
     const links = [...document.getElementsByClassName("internal-link")]
     links
       .filter(li => li.dataset.src || (li.dataset.idx && useContextualBacklinks))
       .forEach(li => {
-        let el
+        var el
         if (li.dataset.ctx) {
           const linkDest = content[li.dataset.src]
           const popoverElement = `<div class="popover">
@@ -27,7 +27,7 @@ function initPopover(baseURL, useContextualBacklinks) {
             let splitLink = li.href.split("#")
             let cleanedContent = removeMarkdown(linkDest.content)
             if (splitLink.length > 1) {
-              let headingName = decodeURIComponent(splitLink[1]).replace(/\-/g, " ")
+              let headingName = splitLink[1].replace(/\-/g, " ")
               let headingIndex = cleanedContent.toLowerCase().indexOf("<b>" + headingName + "</b>")
               cleanedContent = cleanedContent.substring(headingIndex, cleanedContent.length)
             }
@@ -42,7 +42,7 @@ function initPopover(baseURL, useContextualBacklinks) {
 
         if (el) {
           li.appendChild(el)
-          if (LATEX_ENABLED) {
+          if (renderLatex) {
             renderMathInElement(el, {
               delimiters: [
                 { left: '$$', right: '$$', display: false },
@@ -64,11 +64,6 @@ function initPopover(baseURL, useContextualBacklinks) {
             })
 
             el.classList.add("visible")
-            plausible("Popover Hover", {
-              props: {
-                href: li.dataset.src 
-              }
-            })
           })
           li.addEventListener("mouseout", () => {
             el.classList.remove("visible")
